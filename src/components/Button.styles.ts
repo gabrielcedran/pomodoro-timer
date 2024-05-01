9b76ffaf595dcd6eab563d53b3e8d195ffebcd67
Gabriel Cedran <gabrielcedran@gmail.com>
2024-05-01 18:53:42 +0100

    Font and colour scheme definition

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,7 +17,8 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
 
-    background-color: ${props => props.theme.primary};
+    background-color: ${props => props.theme["green-500"]};
+    color: ${props => props.theme.white}
 
     /* ${props => {
         return css`
