3c948a325e3b358c06b4c47ad835d158c84f7bea
Gabriel Cedran <gabrielcedran@gmail.com>
2024-08-07 16:55:01 +0100

    History page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,36 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Tasks History</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Start</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Read about react</td>
+              <td>30 minutes</td>
+              <td>About 30 min ago</td>
+              <td>In progress</td>
+            </tr>
+            <tr>
+              <td>Read about react</td>
+              <td>30 minutes</td>
+              <td>About 1 hour ago</td>
+              <td>Completed</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
