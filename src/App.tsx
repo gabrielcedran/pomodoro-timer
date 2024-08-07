0cc7aaf68b1aa94074cc6e8eb2425396eccf4243
Gabriel Cedran <gabrielcedran@gmail.com>
2024-08-07 11:17:43 +0100

    Styled components first example

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button } from './components/ButtonWithCssModules'
+import { Button } from './components/Button'
 
 export function App() {
   return (
---
+++ src/components/Button/Button.styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
+
+interface ButtonContainerProps {
+  $variant: ButtonVariant
+}
+
+const variantColourMapping = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+  background: ${(props) => {
+    return variantColourMapping[props.$variant]
+  }};
+`
