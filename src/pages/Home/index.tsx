ace0e622bc99feed120562b1764a438521b40230
Gabriel Cedran <gabrielcedran@gmail.com>
2024-08-09 12:57:01 +0100

    Logic to calculate remaining time and render on the ui

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,6 +12,8 @@ import {
   TaskInput,
   TimerSeparator,
 } from './styles'
+import { useEffect, useState } from 'react'
+import { differenceInSeconds } from 'date-fns'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Provide task description'),
@@ -23,7 +25,18 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string
+  task: string
+  durationInMinuts: number
+  startDate: Date
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [activeCycleSecondsElapsed, setActiveCycleSecondsElapsed] = useState(0)
+
   const { register, handleSubmit, watch, reset /* formState */ } =
     useForm<NewCycleFormData>({
       resolver: zodResolver(newCycleFormValidationSchema),
@@ -33,11 +46,53 @@ export function Home() {
       },
     })
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    let setIntervalRef: number
+    if (activeCycle) {
+      setIntervalRef = setInterval(() => {
+        setActiveCycleSecondsElapsed(
+          differenceInSeconds(new Date(), activeCycle.startDate),
+        )
+      }, 1000)
+    }
+    return () => {
+      if (setIntervalRef) {
+        clearInterval(setIntervalRef)
+      }
+    }
+  }, [activeCycle])
+
   function handleCreateNewCycle(data: NewCycleFormData) {
     // formState.errors (to get errors upon submit)
-    console.log(data)
+
+    const newCycle: Cycle = {
+      id: String(new Date().getTime()),
+      task: data.task,
+      durationInMinuts: data.durationInMinutes,
+      startDate: new Date(),
+    }
+    setCycles((currentState) => [...currentState, newCycle])
+    setActiveCycleId(newCycle.id)
+
     reset()
   }
+
+  const activeCycleTotalSeconds = activeCycle
+    ? activeCycle.durationInMinuts * 60
+    : 0
+  const activeCycleTotalSecondsRemaining = activeCycle
+    ? activeCycleTotalSeconds - activeCycleSecondsElapsed
+    : 0
+
+  const activeCycleMinutesReimaining = Math.floor(
+    activeCycleTotalSecondsRemaining / 60,
+  )
+  const activeCycleSecondsRemaining = activeCycleTotalSecondsRemaining % 60
+
+  const currentMinutes = String(activeCycleMinutesReimaining).padStart(2, '0')
+  const currentSeconds = String(activeCycleSecondsRemaining).padStart(2, '0')
 
   // this effectively turns the input into a controlled input, triggering rerender at every key stroke
   const taskInput = watch('task')
@@ -72,11 +127,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{currentMinutes[0]}</span>
+          <span>{currentMinutes[1]}</span>
           <TimerSeparator>:</TimerSeparator>
-          <span>0</span>
-          <span>0</span>
+          <span>{currentSeconds[0]}</span>
+          <span>{currentSeconds[1]}</span>
         </CountdownContainer>
 
         <StartCountdownButton type="submit" disabled={!taskInput}>
